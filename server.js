25d877760c1c7efdd0d9f6ffd82c3fcd3b4181b6
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-24 12:18:53 -0500

    [UPDATED] Basic Packages

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express"),
   app = express();
 
+app.get("/", function (req, res) {
+  res.sendStatus(200);
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(
+    `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
+  );
+});
 
 require(__dirname + "/bot.js")(app);
 
- app.get("/", function(req, res) {
-    res.sendStatus(200);
-    const ping = new Date();
-    ping.setHours(ping.getHours() - 3);
-    console.log(
-      `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
-    );
-  });
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log(`Porta: ${listener.address().port}`);
+});
 
 module.exports = app;
