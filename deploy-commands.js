0a69d72da4a825eed385657af9d0090b9f66bb01
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-24 12:31:08 -0500

    [UPDATE] Deploy Commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,23 @@
-module.exports = client => {
-  const fs = require("fs");
-  const { REST } = require("@discordjs/rest");
-  const { Routes } = require("discord-api-types/v9");
-  const config = require("./config.json");
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('./config.json');
+require('dotenv').config();
 
-  const commands = [];
-  const commandFiles = fs
-    .readdirSync("./commands")
-    .filter(file => file.endsWith(".js"));
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-  }
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
 
-  const rest = new REST({ version: "9" }).setToken(process.env.token);
+const rest = new REST({ version: '9' }).setToken(process.env.PRODTOKEN);
 
-  rest
-    .put(Routes.applicationCommands(config.botConfig.clientId), {
-      body: commands
-    })
-    .then(() => console.log("Sucesso ao registrar [SLASH_COMMANDS]"))
-    .catch(console.error);
-};
+rest.put(Routes.applicationGuildCommands(clientId.prod, guildId), { body: commands })
+	.then(() => console.log('Comandos Registrados com Sucesso'))
+	.catch(console.error);
+  
